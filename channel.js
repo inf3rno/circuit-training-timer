b6cef1107c529bbeee358c5adb16696e67ecc155
inf3rno <i.dont.give.my@email.addr>
2014-08-08 17:31:27 +0200

    Added sequence for unique id generation. Ofc it would be very easy with yield, but that is not available on every platform.

--- channel.js
+++ channel.js
@@ -8,6 +8,52 @@ Channel.prototype = {
     }
 };
 
+var Sequence = function () {
+    this.init.apply(this, arguments);
+};
+Sequence.prototype = {
+    constructor: Sequence,
+    init: function (config) {
+        this.state = config.initial;
+        this.generator = config.generator;
+    },
+    get: function () {
+        return this.state;
+    },
+    next: function () {
+        var args = [this.state];
+        args.push.apply(args, arguments);
+        this.state = this.generator.apply(this, args);
+        return this.get();
+    },
+    wrapper: function () {
+        var store = [];
+        store.push.apply(store, arguments);
+        return function () {
+            var args = [];
+            args.push.apply(args, store);
+            args.push.apply(args, arguments);
+            return this.next.apply(this, args);
+        }.bind(this);
+    }
+};
+
+var uniqueId = new Sequence({
+    generator: function (previous) {
+        return ++previous;
+    },
+    initial: 0
+}).wrapper();
+
+
+(function (helpers) {
+    for (var name in helpers)
+        Channel[name] = helpers[name];
+})({
+    Sequence: Sequence,
+    uniqueId: uniqueId
+});
+
 if (typeof(module) == "object")
     module.exports = Channel;
 else
--- test/channel.spec.js
+++ test/channel.spec.js
@@ -2,9 +2,79 @@ var Channel = require("../channel");
 
 describe("channel.js", function () {
 
-    it("does create a new channel", function () {
-        expect(Channel).toBeDefined();
-        expect(new Channel()).toBeDefined();
+    describe("Sequence", function () {
+
+        it("generates the next state from the previous state", function () {
+            var continuouslyIncreasingSequence = new Channel.Sequence({
+                generator: function (previous) {
+                    return ++previous;
+                },
+                initial: 0
+            });
+            expect(continuouslyIncreasingSequence.get()).toBe(0);
+            expect(continuouslyIncreasingSequence.next()).toBe(1);
+            expect(continuouslyIncreasingSequence.next()).toBe(2);
+            expect(continuouslyIncreasingSequence.next()).toBe(3);
+        });
+
+        it("returns a wrapper which calls next state", function () {
+            var sequence = new Channel.Sequence({
+                generator: function (i) {
+                    return ++i;
+                },
+                initial: 0
+            });
+            var wrapper = sequence.wrapper();
+            expect(wrapper()).toBe(1);
+            expect(wrapper()).toBe(2);
+            expect(wrapper()).toBe(3);
+            expect(sequence.get()).toBe(3);
+        });
+
+        it("accepts additional parameters", function () {
+            var sequence = new Channel.Sequence({
+                generator: function (i, j) {
+                    return i + j;
+                },
+                initial: 0
+            });
+            var wrapper = sequence.wrapper();
+            expect(sequence.next(1)).toBe(1);
+            expect(sequence.next(2)).toBe(3);
+            expect(wrapper(5)).toBe(8);
+        });
+
+        it("stores additional parameters in the wrapper", function () {
+            var sequence = new Channel.Sequence({
+                generator: function (i, j, k) {
+                    return i + j + k;
+                },
+                initial: 0
+            });
+            var wrapper = sequence.wrapper(10);
+            expect(wrapper(5)).toBe(15);
+            expect(sequence.next(1, 2)).toBe(18);
+            expect(wrapper(2)).toBe(30);
+        });
+
+    });
+
+    describe("uniqueId", function (){
+
+        it("returns unique id", function (){
+            expect(Channel.uniqueId()).not.toBe(Channel.uniqueId());
+        });
+
+    });
+
+    describe("Channel", function () {
+
+        it("does create a new channel", function () {
+            expect(Channel).toBeDefined();
+            expect(new Channel()).toBeDefined();
+        });
+
     });
 
 });
+
