1e76faac3df5507bd3093701b55155cabfae5496
inf3rno <i.dont.give.my@email.addr>
2014-08-28 14:59:31 +0200

    Channel is no longer in use, I use DFlo instead of that. #3 The data binding is pretty clear this way, but I moved all of the transformations to the TimerView class. It would be easier with Transformer (Worker in channel.js) instances instead of Publisher + Subscriber to do the transformations, but DFlo currently does not have that feature. Next task by developing DFlo I guess... I think the Timer and the TimerView are both sub-Networks part of the data binding Network, so I think I will be able to display them on a graph with the Publisher and Subscriber property names, but I need a Network class to do that. So adding Transformer, Network, and enhance Traverser and GraphView are the next tasks by the DFlo project...

--- timer.js
+++ timer.js
@@ -1,64 +1,73 @@
-var Timer = function (config) {
-    this.init(config);
-};
-Timer.prototype = {
-    constructor: Timer,
-    interval: null,
-    elapsedTime: null,
-    startDate: null,
-    length: null,
-    tickFrequency: 100,
-    channel: null,
-    init: function (config) {
-        if (config.length)
-            this.setLength(config.length);
-        if (config.tickFrequency)
-            this.tickFrequency = config.tickFrequency;
-        this.channel = new Channel();
-        if (config.events)
-            this.subscribe(config.events);
-    },
-    setLength: function (length) {
-        this.length = length;
-    },
-    subscribe: function (events) {
-        for (var type in events)
-            this.channel.subscribe(
-                new Subscription({
-                    subscriber: (function (type, fn) {
-                        return function (t) {
-                            if (t != type)
-                                return;
-                            var args = [].slice.call(arguments, 1);
-                            fn.apply(null, args);
-                        };
-                    })(type, events[type].bind(this))
-                })
-            );
-    },
-    start: function () {
-        if (this.interval)
+(function (dflo) {
+
+    var Class = dflo.Class;
+    var Publisher = dflo.Publisher;
+    var Subscriber = dflo.Subscriber;
+
+    var Timer = Class.extend({
+        interval: null,
+        elapsedTime: null,
+        startDate: null,
+        started: undefined,
+        ticked: undefined,
+        ended: undefined,
+        cleared: undefined,
+        length: null,
+        tickFrequency: 100,
+        init: function (config) {
+            this.started = new Publisher();
+            this.ticked = new Publisher();
+            this.ended = new Publisher();
+            this.cleared = new Publisher();
+            this.startInitiated = new Subscriber({
+                callback: this.start,
+                context: this
+            });
+            this.stopInitiated = new Subscriber({
+                callback: this.clear,
+                context: this
+            });
+            this.lengthSelected = new Subscriber({
+                callback: this.setLength,
+                context: this
+            });
+            if (!config)
+                return;
+            if (config.length)
+                this.setLength(config.length);
+            if (config.tickFrequency)
+                this.tickFrequency = config.tickFrequency;
+        },
+        setLength: function (length) {
+            this.length = length;
+        },
+        start: function () {
+            if (this.interval)
+                this.clear();
+            this.startDate = new Date();
+            this.started.publish();
+            this.interval = setInterval(this.tick.bind(this), this.tickFrequency);
+            this.tick();
+        },
+        tick: function () {
+            this.elapsedTime = new Date() - this.startDate;
+            if (this.elapsedTime > this.length)
+                this.end();
+            else
+                this.ticked.publish(this.elapsedTime, this.length);
+        },
+        end: function () {
             this.clear();
-        this.startDate = new Date();
-        this.channel.publish("start");
-        this.interval = setInterval(this.tick.bind(this), this.tickFrequency);
-        this.tick();
-    },
-    tick: function () {
-        this.elapsedTime = new Date() - this.startDate;
-        if (this.elapsedTime > this.length)
-            this.end();
-        else
-            this.channel.publish("tick", this.elapsedTime, this.length);
-    },
-    end: function () {
-        this.clear();
-        this.channel.publish("end");
-    },
-    clear: function () {
-        clearInterval(this.interval);
-        this.interval = null;
-        delete(this.elapsedTime);
-        this.channel.publish("clear");
-    }
-};
+            this.ended.publish();
+        },
+        clear: function () {
+            clearInterval(this.interval);
+            this.interval = null;
+            delete(this.elapsedTime);
+            this.cleared.publish();
+        }
+    });
+
+    window.Timer = Timer;
+
+})(window.dflo);
