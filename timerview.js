c8cccb4826ca6b92ae455f7a13fa971365c521fd
inf3rno <i.dont.give.my@email.addr>
2014-09-02 00:22:49 +0200

    Removed the centralized TimerView code. Made a skeleton and a timer-view[] network instead. It is possible to draw a network graph using that, but it would be better with a much simpler code. I'll think about how to make things easier from a developer perspective. For example data binding with lazy init would be nice. Now I have to view.render and timer.clear to lazy init the components. It would be better with a "configure on connect" feature, or something like that... #4

--- timerview.js
+++ timerview.js
@@ -2,10 +2,13 @@
 
     var Class = dflo.Class;
     var uniqueId = dflo.uniqueId;
-    var Subscriber = dflo.Subscriber;
-    var Publisher = dflo.Publisher;
+    var Component = dflo.Component;
+    var OutputPort = dflo.OutputPort;
+    var InputPort = dflo.InputPort;
+    var Message = dflo.Message;
+    var Transformer = dflo.Transformer;
 
-    var LengthSelect = Class.extend({
+    var LengthSelect = Component.extend({
         lengthAlternatives: {
             3600: "1 hour",
             1800: "30 mins",
@@ -22,7 +25,12 @@
         },
         defaultLength: 120,
         init: function () {
-            this.changeLengthOut = new Publisher();
+            Component.prototype.init.apply(this, arguments);
+            this.ports = {
+                selected: new OutputPort({
+                    component: this
+                })
+            };
         },
         render: function () {
             this.el = $("<select>").attr({class: "length"});
@@ -30,20 +38,29 @@
                 this.el.append($("<option>").append(
                     this.lengthAlternatives[length]).attr({value: length, selected: length == this.defaultLength})
                 );
-            this.el.change(function () {
-                this.changeLengthOut.publish(this.el.val() * 1000);
-            }.bind(this));
-            this.changeLengthOut.publish(this.el.val() * 1000);
+            var out = function () {
+                this.ports.selected.relay(new Message(this.el.val() * 1000));
+            }.bind(this);
+            this.el.change(out);
+            out();
         }
     });
 
-    var IntervalCheckbox = Class.extend({
-        init: function (startOut) {
-            this.startOut = startOut;
-            this.ended = function () {
-                if (this.el.prop("checked"))
-                    startOut.publish();
-            }.bind(this)
+    var IntervalCheckbox = Component.extend({
+        init: function () {
+            Component.prototype.init.apply(this, arguments);
+            this.ports = {
+                next: new OutputPort({
+                    component: this
+                }),
+                ended: new InputPort({
+                    component: this,
+                    callback: function () {
+                        if (this.el.prop("checked"))
+                            this.ports.next.relay(new Message());
+                    }.bind(this)
+                })
+            };
         },
         render: function () {
             this.el = $("<input>").attr({class: "interval", type: "checkbox", checked: "checked", id: "chk_id_" + uniqueId()});
@@ -51,168 +68,151 @@
         }
     });
 
-    var StopButton = Class.extend({
-        init: function () {
-            this.stopOut = new Publisher();
+    var Button = Component.extend({
+        init: function (config) {
+            Component.prototype.init.apply(this, arguments);
+            this.ports = {
+                clicked: new OutputPort({
+                    component: this
+                }),
+                enable: new InputPort({
+                    component: this,
+                    callback: function () {
+                        this.el.prop("disabled", false);
+                    }.bind(this)
+                }),
+                disable: new InputPort({
+                    component: this,
+                    callback: function () {
+                        this.el.prop("disabled", true);
+                    }.bind(this)
+                })
+            };
+            this.label = config.label;
+            this.class = config.class;
         },
         render: function () {
-            this.el = $("<button>").attr({"class": "stop"}).append("stop");
+            this.el = $("<button>").attr({"class": this.class}).append(this.label);
             this.el.click(function () {
-                this.stopOut.publish();
+                this.ports.clicked.relay(new Message());
             }.bind(this));
-        },
-        started: function () {
-            this.el.prop("disabled", false);
-        },
-        cleared: function () {
-            this.el.prop("disabled", true);
         }
     });
 
-    var StartButton = Class.extend({
+    var Display = Component.extend({
         init: function () {
-            this.startOut = new Publisher();
-        },
-        render: function () {
-            this.el = $("<button>").attr({"class": "start"}).append("start");
-            this.el.click(function () {
-                this.startOut.publish();
-            }.bind(this));
-        },
-        started: function () {
-            this.el.prop("disabled", true);
-        },
-        cleared: function () {
-            this.el.prop("disabled", false);
-        }
-    });
-
-
-    var Display = Class.extend({
-        init: function () {
-
+            Component.prototype.init.apply(this, arguments);
+            this.ports = {
+                text: new InputPort({
+                    component: this,
+                    callback: function (message) {
+                        this.el.text(message.data[0]);
+                    }.bind(this)
+                }),
+                clear: new InputPort({
+                    component: this,
+                    callback: function () {
+                        this.el.text("-");
+                    }.bind(this)
+                })
+            };
         },
         render: function () {
             this.el = $("<section>").attr({class: "display"});
-        },
-        ticked: function (timeText) {
-            this.el.text(timeText);
-        },
-        cleared: function () {
-            this.el.text("-");
         }
     });
 
-    var Title = Class.extend({
+    var Title = Component.extend({
         init: function () {
-
-        },
-        ticked: function (timeText) {
-            $(document).prop("title", "timer " + timeText);
-        },
-        cleared: function () {
-            $(document).prop("title", "timer -");
+            Component.prototype.init.apply(this, arguments);
+            this.ports = {
+                text: new InputPort({
+                    component: this,
+                    callback: function (message) {
+                        $(document).prop("title", "timer " + message.data[0]);
+                    }
+                }),
+                clear: new InputPort({
+                    component: this,
+                    callback: function () {
+                        $(document).prop("title", "timer -");
+                    }
+                })
+            };
         }
     });
 
-    var BeepAudio = Class.extend({
-        beepAudioSource: "vendor/beep-07.wav",
-        init: function (){
-
-        },
-        render: function (){
-            this.el = $("<audio>").attr({class: "beep", src: this.beepAudioSource});
-        },
-        ended: function (){
-            var beep = this.el.get(0);
-            beep.pause();
-            beep.currentTime = 0;
-            beep.play();
-        }
-    });
-
-    var TimeToText = Class.extend({
-        init: function (){
-
-        },
-        make: function (elapsedTime, length){
-            var remainingTime = length - elapsedTime;
-            var timeText = "";
-            var remainingSeconds = Math.floor(remainingTime / 1000) % 60;
-            var remainingMinutes = Math.floor(remainingTime / 1000 / 60);
-            if (remainingMinutes > 9)
-                timeText += String(remainingMinutes);
-            else
-                timeText += "0" + String(remainingMinutes);
-            timeText += ":";
-            if (remainingSeconds > 9)
-                timeText += String(remainingSeconds);
-            else
-                timeText += "0" + String(remainingSeconds);
-            return timeText;
-        }
-    });
-
-    window.TimerView = Class.extend({
-        init: function () {
-            this.startedIn = new Subscriber({callback: this.started, context: this});
-            this.tickedIn = new Subscriber({callback: this.ticked, context: this});
-            this.clearedIn = new Subscriber({callback: this.cleared, context: this});
-            this.endedIn = new Subscriber({callback: this.ended, context: this});
-
-            this.beepAudio = new BeepAudio();
-
-            this.title = new Title();
-            this.display = new Display();
-
-            this.startButton = new StartButton();
-            this.startOut = this.startButton.startOut;
-
-            this.stopButton = new StopButton();
-            this.stopOut = this.stopButton.stopOut;
-
-            this.intervalCheckbox = new IntervalCheckbox(this.startOut);
-
-            this.lengthSelect = new LengthSelect();
-            this.changeLengthOut = this.lengthSelect.changeLengthOut;
+    var AudioFile = Component.extend({
+        init: function (config) {
+            Component.prototype.init.apply(this, arguments);
+            this.ports = {
+                replay: new InputPort({
+                    component: this,
+                    callback: function () {
+                        var beep = this.el.get(0);
+                        beep.pause();
+                        beep.currentTime = 0;
+                        beep.play();
+                    }.bind(this)
+                })
+            };
+            this.class = config.class;
+            this.source = config.source;
         },
         render: function () {
-            this.startButton.render();
-            this.stopButton.render();
-            this.intervalCheckbox.render();
-            this.lengthSelect.render();
-            this.display.render();
-
-            this.beepAudio.render();
-
-            this.el = $("<section>").attr({class: "timer"}).append(
-                $("<section>").attr({class: "config"}).append(this.lengthSelect.el, this.intervalCheckbox.el, this.intervalCheckbox.label),
-                this.display.el,
-                $("<section>").attr({class: "controllers"}).append(this.startButton.el, this.stopButton.el),
-                $("<section>").attr({class: "sounds"}).append(this.beepAudio.el)
-            );
-            return this;
-        },
-        started: function () {
-            this.startButton.started();
-            this.stopButton.started();
-        },
-        ticked: function (elapsedTime, length) {
-            var time2Text = new TimeToText();
-            var timeText = time2Text.make(elapsedTime, length);
-            this.display.ticked(timeText);
-            this.title.ticked(timeText);
-        },
-        cleared: function () {
-            this.startButton.cleared();
-            this.stopButton.cleared();
-            this.display.cleared();
-            this.title.cleared();
-        },
-        ended: function () {
-            this.beepAudio.ended();
-            this.intervalCheckbox.ended();
+            this.el = $("<audio>").attr({class: this.class, src: this.source});
         }
     });
+
+    var Time2Text = Transformer.extend({
+        init: function () {
+            Transformer.prototype.init.call(this, {
+                callback: function (data, done) {
+                    var elapsedTime = data[0];
+                    var length = data[1];
+                    var remainingTime = length - elapsedTime;
+                    var timeText = "";
+                    var remainingSeconds = Math.floor(remainingTime / 1000) % 60;
+                    var remainingMinutes = Math.floor(remainingTime / 1000 / 60);
+                    if (remainingMinutes > 9)
+                        timeText += String(remainingMinutes);
+                    else
+                        timeText += "0" + String(remainingMinutes);
+                    timeText += ":";
+                    if (remainingSeconds > 9)
+                        timeText += String(remainingSeconds);
+                    else
+                        timeText += "0" + String(remainingSeconds);
+                    done([timeText]);
+                }
+            });
+        }
+    });
+
+    var SkeletonView = Class.extend({
+        init: function () {
+
+        },
+        render: function () {
+            this.el = $("<section>").attr({class: "timer"});
+            this.config = $("<section>").attr({class: "config"});
+            this.display = $("<section>");
+            this.controllers = $("<section>").attr({class: "controllers"});
+            this.sounds = $("<section>").attr({class: "sounds"});
+            this.el.append(this.config, this.display, this.controllers, this.sounds);
+            return this;
+        }
+    });
+
+    window.view = {
+        SkeletonView: SkeletonView,
+        Button: Button,
+        Title: Title,
+        Display: Display,
+        AudioFile: AudioFile,
+        IntervalCheckbox: IntervalCheckbox,
+        LengthSelect: LengthSelect,
+        Time2Text: Time2Text
+    };
 
 })(window.dflo);
